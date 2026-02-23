7b3ca3b2d61dfb4dbdc50ae17033d8ddb3ecf71e
Udaykiran218 <udaykiran2618@gmail.com>
2026-02-23 23:09:11 +0530

    Add files via upload

---
+++ prime no.py
@@ -0,0 +1,19 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def find_primes(start, end):
+    primes = []
+    for num in range(start, end + 1):
+        if is_prime(num):
+            primes.append(num)
+    return primes
+
+
+print(find_primes(1, 10))  # Output: [2, 3, 5, 7]
