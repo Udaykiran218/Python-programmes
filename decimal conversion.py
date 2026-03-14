8ba5281019fa38f0399bc70869bbe3d8b14a8a9e
Udaykiran218 <udaykiran2618@gmail.com>
2026-03-14 23:05:59 +0530

    Add files via upload

---
+++ decimal conversion.py
@@ -0,0 +1,6 @@
+# Python program to convert decimal into other number systems
+decimal = int(input("enter decimal number :"))
+print("The decimal value is:", decimal)
+print(bin(decimal), "in binary.")
+print(oct(decimal), "in octal.")
+print(hex(decimal), "in hexadecimal.")
