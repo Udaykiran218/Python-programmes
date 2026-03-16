444767f249d2e501706f4bddd76508910c517bc8
Udaykiran218 <udaykiran2618@gmail.com>
2026-03-16 21:17:21 +0530

    Add files via upload

---
+++ username password.py
@@ -0,0 +1,9 @@
+username1 = input (" your username : ")
+password1 = input (" your password : ")
+username = input ("enter username :")
+password = input ("enter password :")
+if(username ==username1 and password ==password1):
+    print("login succesful ")
+else:
+    print("invalid details")    
+    
