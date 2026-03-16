9dbc9e288d423c5e7638c092d33b9274056940b1
Udaykiran218 <udaykiran2618@gmail.com>
2026-03-16 21:17:46 +0530

    Add files via upload

---
+++ hallow butterfly.py
@@ -0,0 +1,29 @@
+rows = int(input("Enter the row size for the pattern: "))
+for i in range(1, rows + 1):  # Upper part of butterfly
+    for j in range(1, i + 1):
+        if j == 1 or j == i:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    for j in range(2 * (rows - i)):
+        print(" ", end=" ")
+    for j in range(1, i + 1):
+        if j == 1 or j == i:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+for i in range(rows, 0, -1):  # Lower part of butterfly
+    for j in range(1, i + 1):
+        if j == 1 or j == i:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    for j in range(2 * (rows - i)):
+        print(" ", end=" ")
+    for j in range(1, i + 1):
+        if j == 1 or j == i:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
